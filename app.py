27ff6590b0034f365226f7f970da8e9751750324
= <=>
2025-11-17 17:13:22 -0700

    17nov

--- app.py
+++ app.py
@@ -3,36 +3,38 @@ import requests
 
 app = Flask(__name__)
 app.secret_key = "tu_clave"
-API = "https://world.openfoodfacts.org/api/v0/product/"
+API = "https://world.openfoodfacts.org/api/v2/product/"
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/search', methods=['POST']) 
-def search_Open():
-    producto = request.form.get('barcode','').strip().lower()
+def search_open():
+    barcode = request.form.get('barcode','').strip()
     
-    if not producto:
-        flash('por favor ingresa un producto','error')
+    if not barcode:
+        flash('por favor ingresa un codigo de barras','error')
         return redirect(url_for('index'))
     
     try:
-        resp = requests.get(f"{API}{producto}.json")
+        resp = requests.get(f"{API}{barcode}.json")
         if resp.status_code == 200:
-            pokemon = resp.json()
-            pokemon_info= {
-    'name' : producto.get['name'].title(),
-    'id' : producto.get['id'],
-    'height' : producto.get['height'] /10,
-    'weight' : producto.get['weight'] /10,
-    'image' : producto.get['sprites']['front_default'],
-    'types' : [t['type']['name'].title() for t in producto.get['types']],
-    'abilities' : [a['ability']['name'].title() for a in producto.get['abilities']],
-    'stats' : {}
+            data = resp.json()
+            if "product" not in data:
+                flash(f'Producto no encontrado', 'error')
+                return redirect(url_for('index'))
+            product = data["product"]
+            product_info = {
+    "name" : product.get("product_name","Desconocido"),
+    "brand" : product.get("brands", "Desconocido"),
+    "quantity" : product.get("quantity","Desconocido"),
+    "calories" : product.get("nutriments", {}).get("energy-kcal", "N/A"),
+    "fat" : product.get("nutriments", {}).get("fat", "N/A"),
+    'image' : product.get("image_front_url", ""),
 }
-            return render_template('pokemon.html', pokemon=pokemon_info)
+            return render_template('Open.html', product=product_info)
         else:
-            flash(f'Producto "{producto}"no encontrado', 'error')
+            flash(f'Producto no encontrado', 'error')
             return redirect(url_for('index'))
     except requests.exceptions.RequestException as e:
         flash('Error al buscar el producto','error')
